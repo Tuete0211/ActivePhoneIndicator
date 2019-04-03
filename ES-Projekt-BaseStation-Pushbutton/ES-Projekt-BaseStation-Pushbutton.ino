d8101538f0e639ef7f5695ad4c76a7d27aa80134
Fiete Hiersig <fiete.hiersig@gmail.com>
2019-04-03 20:54:19 +0200

    * added StartUp sequence for Receiver * reformat code * documentation

--- ES-Projekt-BaseStation-Pushbutton/ES-Projekt-BaseStation-Pushbutton.ino
+++ ES-Projekt-BaseStation-Pushbutton/ES-Projekt-BaseStation-Pushbutton.ino
@@ -1,46 +1,48 @@
 /**
- * ISDN Phone Sniffer
- * 
+ * Transmitter with a reed-switch as a trigger
+ *
  * Purpose:
- *    Detecting if Phone is activ (dial-tone is sounding)
- *    If phone is on ISDN-Bus all Phones will have effect on the sign
- *  
+ *    Detecting if Phone is activ (Reed-switch open)
+ *    Send four times a signal on reed-switch change
  *  How it works:
- *    By measuring the voltage of the Datalines (3 & 6) a specific pattern is measurable.
- *    The signal is devided in Spikes (over 160), Data-Packages (btween 100 and 180) and noise.
- *    With the handset in its place on the phone no Data-Packages are recordable. 
- *    With the handset lifted and the dial-tone sounding Data-Packages are measurable between Spikes.
- *    The Spikes are measurable always.
- * 
-*/
+ *    With the handset in its place on the phone the reed-switch is closed due to a magnet on the headset.
+ *    With the handset lifted the switch is open.
+ */
 
 #include <RCSwitch.h>
 
+#define DEBUG 0
+#define TRANSMITTER_PIN 10
+#define INPUT_PIN 2
+#define LED_PIN 6
+
+//variables for transmitter
 RCSwitch mySwitch = RCSwitch();
-const short transmitterPin = 10;
-int startSignal = 1234;
-int stopSignal = 5678;
+int startSignal = 13127467;
+int stopSignal = 36653082;
 
-const short inputPinNumber = 2;
-const short ledPin = 6;
-
-unsigned long oldMillis = 0;
+long oldMillis = 0;
 bool phoneLifted = false;
 bool ledActive = false;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.println("Start...");
-  pinMode(ledPin, OUTPUT);
-  mySwitch.enableTransmit(transmitterPin);
+  delay( 3000 ); // power-up safety delay
+  if (DEBUG) {
+    Serial.begin(9600);
+    Serial.println("Start...");
+  }
+
+  //setup indicator LED
+  pinMode(LED_PIN, OUTPUT);
+
+  //setup transmitter
+  mySwitch.enableTransmit(TRANSMITTER_PIN);
 }
 
 void loop() {
-
-  // put your main code here, to run repeatedly:
-  short digitalInput = digitalRead(inputPinNumber);
-  Serial.println(digitalInput);
+  // check INPUT_PIN for current value
+  short digitalInput = digitalRead(INPUT_PIN);
+  if(DEBUG)Serial.println(digitalInput);
 
   if(digitalInput == 0) {
     phoneLifted = true;
@@ -50,16 +52,16 @@ void loop() {
 
   if (oldMillis + 1000 < millis()) { // every 1000ms the LED can be changed
     if (phoneLifted != ledActive) {
-      digitalWrite(ledPin, phoneLifted ? HIGH : LOW);
+      digitalWrite(LED_PIN, phoneLifted ? HIGH : LOW);
       for (int i = 0; i<5; i++) {
         if (phoneLifted) {
           mySwitch.send(startSignal, 24);
           delay(1);
-          Serial.println("startSignal");
+          if(DEBUG)Serial.println("startSignal");
         } else {
           mySwitch.send(stopSignal, 24);
           delay(1);
-          Serial.println("stopSognal");
+          if(DEBUG)Serial.println("stopSognal");
         }
       }
       ledActive = phoneLifted;
--- ES-Projekt-Receiver/ES-Projekt-Receiver.ino
+++ ES-Projekt-Receiver/ES-Projekt-Receiver.ino
@@ -1,26 +1,23 @@
 /**
-   Simple Receiver
-   Purpose:
-      Move a Servo up or down
-      Turn on an off LED Strip
-      Give the LED Strip a simple color pattern
-   How it works:
-      By checking if a message from an 433 MHz receiver is available a global variable is set.
-      It the variable is true the servo turns and the LED Strip is activated.
-      If the variable is false the servo is turning in th eopposite direction and the LED Strip is reset.
-      To secure the servo maximal and minimal values have to be set
-      To control the LED Strip the library FastLED is used
-   Todo:
-      FastLED library
-      connecting an LED Strip
-*/
+ * Receiver with indicator light and movable handle
+ *
+ * Purpose:
+ *    Move a Servo up or down
+ *    Turn on and off LED Strip
+ *    Give the LED Strip a simple pattern on booting
+ * How it works:
+ *    By checking if a message from an 433 MHz receiver is available and right, a global variable is set.
+ *    If the variable is true the servo turns and the LED Strip is activated.
+ *    If the variable is false the servo is turning in the opposite direction and the LED Strip is reset.
+ *    To secure the servo, maximal and minimal values have to be set
+ *    To control the LED Strip the library 'FastLED' is used
+ */
 
 #include <RCSwitch.h>
 #include <Servo.h>
 #include <FastLED.h>
 
-#define RAINBOW 0
-#define DEBUG 1
+#define DEBUG 0
 #define RECEIVER_PIN 0
 #define SERVO_PIN 9
 #define LED_PIN 6
@@ -28,12 +25,12 @@
 
 //variables for receiver
 RCSwitch mySwitch = RCSwitch();
-int startSignal = 1234;
-int stopSignal = 5678;
+int startSignal = 13127467;
+int stopSignal = 36653082;
 
 //variables for servo
-Servo myServo;  // create servo object to control a servo
-float myServoPos = 0;    // variable to store the servo position
+Servo myServo;          // create servo object to control a servo
+float myServoPos = 0;   // variable to store the servo position
 int upPos = 10;
 int downPos = 40;
 
@@ -45,26 +42,30 @@ bool currentState = false;
 
 void setup() {
   delay( 3000 ); // power-up safety delay
-
   if (DEBUG) {
     Serial.begin(9600);
     Serial.println("Start");
   }
+
   //setup receiver
   mySwitch.enableReceive(RECEIVER_PIN);  // Receiver on interrupt 0 => that is pin #2
-
-  // setup servo
-  myServo.attach(SERVO_PIN);  // attaches the servo on pin 9 to the servo object
-  myServo.write(0);
-  delay(2000);
-  myServo.write(50);
-  delay(2000);
-  myServo.write(10);
-  delay(2000);
 
   //setup LED strip
   LEDS.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   LEDS.setBrightness(84);
+
+  bool startUpFinished = false;
+  long stopTime = millis() + 5000;
+  while (!startUpFinished) {
+    lightOn(true);
+    startUpFinished = millis() > stopTime;
+  }
+  lightOff();
+
+  // setup servo
+  myServo.attach(SERVO_PIN);  // attaches the servo on pin 9 to the servo object
+  myServo.write(downPos);
+  delay(1000);
 
   if(DEBUG)Serial.println("setup finish");
 }
@@ -88,7 +89,7 @@ void loop() {
 
   if (activeCall != currentState) {
     if (activeCall) {
-      lightOn();
+      lightOn(false);
       moveHandleUp();
       if(DEBUG)Serial.print("active Call");
     } else {
@@ -100,6 +101,9 @@ void loop() {
   }
 }
 
+/**
+ * Move phone handle up
+ */
 void moveHandleUp() {
   if (myServoPos != upPos) {
     myServoPos = upPos;
@@ -108,6 +112,9 @@ void moveHandleUp() {
   }
 }
 
+/**
+ * Move phone handle down
+ */
 void moveHandleDown() {
   if (myServoPos != downPos) {
     myServoPos = downPos;
@@ -116,48 +123,55 @@ void moveHandleDown() {
   }
 }
 
-void lightOn() {
-  if (RAINBOW) {
+/**
+ * Turn on LED Strip
+ */
+void lightOn(bool startup) {
+  if (startup) {
     static uint8_t startIndex = 0;
-    startIndex = startIndex + 4; /* motion speed */
-    FillLEDsFromPaletteColors( startIndex);
+    startIndex = startIndex + 1; /* motion speed */
+    FillLEDsFromPaletteColors(startIndex);
   } else {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i].setRGB( 0, 155, 220);
-    }
+    fill_solid(leds, NUM_LEDS, CRGB(0, 155, 220));
   }
   FastLED.show();
-  //FastLED.delay(1000 / UPDATES_PER_SECOND);
 }
 
+/**
+ * Clear whole LED Strip
+ */
 void lightOff() {
-  if (leds[0].r != 0) {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB::Black;
-      //leds[i].nscale8(200);
-    }
-    FastLED.show();
-  } else if (leds[0].g != 0) {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB::Black;
-      //leds[i].nscale8(200);
-    }
-    FastLED.show();
-  } else if (leds[0].b != 0) {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB::Black;
-      //leds[i].nscale8(200);
-    }
-    FastLED.show();
-  }
+  FastLED.clear();
+  FastLED.show();
 }
 
+/**
+ * A way to make pattern move along the LEDs
+ */
 void FillLEDsFromPaletteColors( uint8_t colorIndex)
 {
   uint8_t brightness = 255;
 
   for ( int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = ColorFromPalette( RainbowColors_p, colorIndex, brightness, LINEARBLEND);
-    colorIndex += 3;
+    leds[i] = ColorFromPalette( SetupBlackAndWhiteStripedPalette(), colorIndex, brightness, LINEARBLEND);
+    colorIndex += 1;
   }
+}
+
+/**
+ * FastLEDs way of creating color pattern
+ */
+CRGBPalette16 SetupBlackAndWhiteStripedPalette()
+{
+  CRGBPalette16 currentPalette;
+  // 'black out' all 16 palette entries...
+  fill_solid( currentPalette, 16, CRGB::Black);
+  // and set every fourth one to white.
+  currentPalette[0] = CRGB::White;
+  currentPalette[1] = CRGB::White;
+  currentPalette[2] = CRGB::White;
+  currentPalette[3] = CRGB::White;
+  currentPalette[4] = CRGB::White;
+  currentPalette[5] = CRGB::White;
+  return currentPalette;
 }
